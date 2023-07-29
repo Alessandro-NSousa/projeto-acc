fa0932bd266bb86c9499c1470b2e92ffd004414c
Alessandro-NSousa <alessandro.sousa@castanhal.ufpa.br>
2023-07-29 00:09:50 -0300

    atualização do footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  faHome = faHome;
+}
